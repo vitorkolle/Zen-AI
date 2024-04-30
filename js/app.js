220bf78af705531deee7660bfedb3cd7152b03c0
VITOR PAES KOLLE <23111830@CFP127ALUNO.LOCAL>
2024-04-30 09:31:28 -0300

    validação de usuário pronta

--- js/app.js
+++ js/app.js
@@ -1,16 +1,34 @@
 'use strict'
 
+const inputUsuario = document.getElementById('usuario')
+const inputSenha = document.getElementById('senha')
+
 const btn = document.getElementById('entrar')
 btn.addEventListener('click', validarLogin)
 
-function validarLogin(){
+inputUsuario||inputSenha.addEventListener('keypress', function(event){
+    if(event.key == 'Enter'){
+        validarLogin()
+    }
+})
+
+async function validarLogin() {
+    const url = 'https://back-login.vercel.app/usuarios'
+    const response = await fetch(url)
+    const usuarios = await response.json()
+
     const usuario = document.getElementById('usuario').value
     const senha = document.getElementById('senha').value
 
-    if(usuario == 'usuario' && senha == '1234'){
-        window.location.href = 'home.html'
-    }else{
-        alert('Usuário e/ou senha inválidos!!!')
-        location.reload()
+    if(usuario == '' || senha == ''){
+        alert('Preencha todos os campos!!')
     }
+
+    usuarios.forEach(element => {
+        if (usuario === element.nome && senha === element.senha) {
+            alert('Usuário logado com sucesso!!!')
+            window.location.href = '../home.html'
+        }
+    });
+
 }
